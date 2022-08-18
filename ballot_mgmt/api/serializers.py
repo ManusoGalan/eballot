7cbcf7b2a2bf7dd0f6866bf376252f5642fa452b
Manuel Galán <manugalangarcia@gmail.com>
2022-08-18 21:03:32 +0200

    feat: Added fields to old serializers and new ones
    Added extra fields, which require some logic, to old serilizers
    Added new serializers according to class diagram

--- ballot_mgmt/api/serializers.py
+++ ballot_mgmt/api/serializers.py
@@ -5,11 +5,29 @@ from rest_framework import serializers
 from .models import BallotBox, Candidate
 
 class BallotBoxSerializer(serializers.ModelSerializer):
+    candidates = serializers.SerializerMethodField()
     class Meta:
         model = BallotBox
-        fields = ['id', 'name', 'initTimestamp', 'endTimestamp', 'contractAddress']
+        fields = ['id', 'name', 'initTimestamp', 'endTimestamp', 'candidates']
         
+    def get_candidates(self, ballot):
+        candidateQuery = Candidate.objects.filter(ballotParent = ballot)
+        return SimpleCandidateSerializer(candidateQuery, many = True).data
+        
+class BallotBoxContractAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BallotBox
+        fields = ['contractAddress']
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
-        fields = ['id', 'name', 'imgPath', 'description', 'website', 'moto']
+        fields = ['id', 'name', 'imgPath', 'description', 'website', 'motto', 'ballotParent']
+        
+class SimpleCandidateSerializer(serializers.ModelSerializer):
+    result = serializers.SerializerMethodField()
+    class Meta:
+        model = Candidate
+        fields = ['id', 'name', 'result']
+        
+    def get_result(self, candidate):
+        return 100
