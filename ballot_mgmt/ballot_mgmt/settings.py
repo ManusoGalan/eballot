ca8d422a2d3ee6779087d41547e73ff9c4c730be
Manuel Galán <manugalangarcia@gmail.com>
2022-08-18 21:23:52 +0200

    fix: Fixed SECRET_KEY leak with new secret
    Moved SECRET_KEY to env file and retrieve new key from there

--- ballot_mgmt/ballot_mgmt/settings.py
+++ ballot_mgmt/ballot_mgmt/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ktfihf)o5_yu(p(4k6olt#$f05)na61=209mcp)#g)xp!t)r1+'
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
