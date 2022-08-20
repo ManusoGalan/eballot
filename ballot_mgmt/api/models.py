89bbb7f6b7b33bfff5ac6583656e2a1411f38d89
Manuel Galán <manugalangarcia@gmail.com>
2022-08-20 23:39:23 +0200

    refactor: Change variable name
    Change variable id_for_ballot to pk_inside_ballot for better reading

--- ballot_mgmt/api/models.py
+++ ballot_mgmt/api/models.py
@@ -13,9 +13,17 @@ class BallotBox(models.Model):
     
 class Candidate(models.Model):
     id = models.BigAutoField(primary_key=True)
+    pk_inside_ballot = models.IntegerField()
     name = models.CharField(max_length=32)
     img_path = models.ImageField()
     description = models.CharField(max_length=500)
     website = models.URLField(blank=True, null=True)
     motto = models.CharField(max_length=100, blank=True, null=True)
     ballot_parent = models.ForeignKey(to=BallotBox, on_delete=models.CASCADE)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['pk_inside_ballot', 'ballot_parent'], name='unique_pk_inside_ballot_ballot'
+            )
+        ]
